b2792545cbc3160dfa831a2eb1962929d272a6ca
baig78 <mirzanaimathbaig@gmail.com>
2023-12-29 18:02:12 +0530

    started home component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,50 @@
 import React from "react";
 import "./Header.css";
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../../assets/consaint_site_logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar">
-      <h3 className="logo">Logo</h3>
-
-      <ul className="nav-links">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/service">
-          <li>Service</li>
-        </Link>
-        <Link to="/industries">
-          <li>Industries</li>
-        </Link>
-        <Link to="/carrier">
-          <li>Carrier</li>
-        </Link>
-        <Link to="/news">
-          <li>News</li>
-        </Link>
-        <Link to="/company">
-          <li>Company</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact Us</li>
-        </Link>
-      </ul>
-    </nav>
+    <Navbar
+      expand="lg"
+      sticky="top"
+      className="shadow p-3 mb-5 bg-body rounded"
+    >
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} className="site-logo" alt="consaint" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Link to="/" className="nav-link">
+              <li>Home</li>
+            </Link>
+            <Link to="/service" className="nav-link">
+              <li>Service</li>
+            </Link>
+            <Link to="/industries" className="nav-link">
+              <li>Industries</li>
+            </Link>
+            <Link to="/carrier" className="nav-link">
+              <li>Carrier</li>
+            </Link>
+            <Link to="/news" className="nav-link">
+              <li>News</li>
+            </Link>
+            <Link to="/company" className="nav-link">
+              <li>Company</li>
+            </Link>
+            <Link to="/contact" className="nav-link">
+              <li>Contact Us</li>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import HeroImage from "../../assets/hero_image.png";
+import "../home/Home.css";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      {/* <Row>
+        <Col>
+          <h1>Contrary to popular belief, Lorem Ipsum</h1>
+        </Col>
+        <Col>
+          <img src={HeroImage} alt="hero" />
+        </Col>
+      </Row> */}
+      <div className="hero-section">
+        <div className="text-container">
+          <h1>Contrary to popular belief, Lorem Ipsum</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500.
+          </p>
+          <Button className="enqurebtn rounded-pill">Enquire Now</Button>
+          <Button className="contactbtn rounded-pill ">Contact Us</Button>
+        </div>
+        <div className="image-container">
+          <img src={HeroImage} alt="hero" />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
